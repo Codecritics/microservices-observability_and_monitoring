60ffcbf40bbd5b79598c6662ae5c3284475768bf
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-07-20 15:51:29 +0200

    trying to fix backend

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
@@ -1,64 +1,69 @@
+from flask import Flask, render_template, request, jsonify
+
+import pymongo
 import logging
-
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from flask_opentracing import FlaskTracing
 from flask_pymongo import PyMongo
-from jaeger_client import Config
-from opentelemetry import trace
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    ConsoleSpanExporter,
-    SimpleExportSpanProcessor,
-)
+from flask_cors import CORS
+
 from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
-logging.basicConfig(level=logging.INFO)
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+from flask_opentracing import FlaskTracing
 
-trace.set_tracer_provider(TracerProvider())
-trace.get_tracer_provider().add_span_processor(
-    SimpleExportSpanProcessor(ConsoleSpanExporter())
-)
+
+## jaeger
+def config_tracer():
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+
+            },
+            'logging': True,
+        },
+        service_name="service_backend",
+        validate=True,
+        metrics_factory=PrometheusMetricsFactory(service_name_label="service_backend")
+    )
+    return config.initialize_tracer()
+
+
+## jaeger
 
 app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
+
+metrics = GunicornInternalPrometheusMetrics(app)
+CORS(app)
+
+jaeger_tracer = config_tracer()
+tracing = FlaskTracing(jaeger_tracer, False, app)
 
 app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 
 mongo = PyMongo(app)
-metrics = PrometheusMetrics(app)
 
-metrics.info("app_info", "Backend", version="1.0.0")
-CORS(app)
-
-config = Config(
-    config={
-        'sampler':
-            {'type': 'const',
-             'param': 1},
-        'logging': True,
-        'reporter_batch_size': 1, },
-    service_name="service")
-jaeger_tracer = config.initialize_tracer()
-tracing = FlaskTracing(jaeger_tracer, True, app)
+logging.basicConfig(level=logging.INFO)
 
 
 @app.route('/')
+@tracing.trace()
 def homepage():
     return "Hello World"
 
 
 @app.route('/api')
+@tracing.trace()
 def my_api():
     answer = "something"
     return jsonify(repsonse=answer)
 
 
 @app.route('/star', methods=['POST'])
+@tracing.trace()
 def add_star():
     star = mongo.db.stars
     name = request.json['name']
@@ -70,4 +75,4 @@ def add_star():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
